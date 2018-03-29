bf7079169436eb911ba4eb8fd3df0573f417c256
Natalia Maystrenko <nataliamaystrenko@Natalias-MacBook-Pro.local>
2018-03-29 23:20:01 +0300

    added fixes in first task

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
+const NEXT_KEYCODE = 39;
+const PREV_KEYCODE = 37;
+
 const allElements = Array.from(document.getElementsByTagName(`template`));
 
-const allElementsId = allElements.map(function (element) {
-  return element.id;
-});
-
-const appPageShow = function (i) {
-  const appPageTemplate = document.querySelector(`#` + allElementsId[i]).content;
+const appPageShow = (i) => {
+  const appPageTemplate = allElements[i].content;
   const appPage = appPageTemplate.cloneNode(true);
   const appContainer = document.querySelector(`.central`);
   appContainer.innerHTML = ``;
@@ -14,25 +13,21 @@ const appPageShow = function (i) {
 
 let currentPage = 0;
 
-
-function showNextPage() {
-  if (currentPage < allElementsId.length - 1) {
+const showNextPage = () => {
+  if (currentPage < allElements.length - 1) {
     currentPage = currentPage + 1;
     appPageShow(currentPage);
   }
-}
+};
 
-function showPrevPage() {
+const showPrevPage = () => {
   if (currentPage > 0) {
     currentPage = currentPage - 1;
     appPageShow(currentPage);
   }
-}
+};
 
-const NEXT_KEYCODE = 39;
-const PREV_KEYCODE = 37;
-
-document.addEventListener(`keydown`, function (e) {
+document.addEventListener(`keydown`, (e) => {
   if (e.keyCode === NEXT_KEYCODE && e.altKey) {
     showNextPage();
   } else if (e.keyCode === PREV_KEYCODE && e.altKey) {
