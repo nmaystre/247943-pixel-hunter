8c482afb9c89d24bd11dd4111e1a96fb17886d4a
Natalia Maystrenko <nataliamaystrenko@Natalias-MacBook-Pro.local>
2018-04-09 11:02:45 +0300

    исправила переключение слайдов

--- js/rules.js
+++ js/rules.js
@@ -7,7 +7,7 @@ const template = getElementFromTemplate(`<div id="rules">
   <header class="header">
     <div class="header__back">
       <button class="back">
-        <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+        <img src="img/arnmprow_left.svg" width="45" height="45" alt="Back">
         <img src="img/logo_small.svg" width="101" height="44">
       </button>
     </div>
@@ -47,7 +47,8 @@ export default () => {
       currentPage.querySelector(`.continue`).disabled = false;
     }
   });
-  currentPage.querySelector(`.continue`).addEventListener(`click`, () => {
+  currentPage.querySelector(`.rules__form`).addEventListener(`submit`, (e) => {
+    e.preventDefault();
     appPageShow(game1());
   });
   currentPage.querySelector(`.back`).addEventListener(`click`, () => {
